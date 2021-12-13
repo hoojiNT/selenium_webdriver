11ff4dd120332a877f42446502ee79c69d17abad
hooji <thehoinguyen@outlook.com>
2021-12-14 00:01:45 +0700

    Add project files.

---
+++ selenium_webdriver/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace selenium_webdriver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpUser = "LoginControl1_txtusername";
+            string inpPassword = "LoginControl1_txtpassword";
+            string btnDangNhap = "LoginControl1_btnDangNhap";
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://thongtin.dainam.edu.vn/";
+            IWebElement txtUsername = driver.FindElement(By.Id(inpUser));
+            txtUsername.SendKeys("1251020103");
+            IWebElement txtPassword = driver.FindElement(By.Id(inpPassword));
+            txtPassword.SendKeys("Binh2311");
+            IWebElement btnDang = driver.FindElement(By.Id(btnDangNhap));
+            btnDang.Click();
+        }
+    }
+}
